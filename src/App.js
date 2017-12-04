d8c02baa6de47e7f5534b5ae7c439afba4042c3e
Rob Shelly <robshelly89@gmail.com>
2017-12-04 00:35:43 +0000

    Demo CI/CD

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          This is just a Sample React App used to demo CI/CD!
+          This is just a Sample React App!
         </p>
       </div>
     );
